b23cf18aa150dcf4b5772d13aca6f0f0490a774a
Ruben <sneexy@disroot.org>
2025-12-09 01:07:09 -0600

    refactor: better scripts structure

--- files/scripts/additions/just.sh
+++ files/scripts/additions/just.sh
@@ -3,9 +3,7 @@
 
 set -euox pipefail
 
-#
 # Justfiles
-#
 wget "https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/share/ublue-os/just/82-bazzite-waydroid.just" -O "/usr/share/ublue-os/just/82-waydroid.just"
 wget "https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/share/ublue-os/just/81-bazzite-fixes.just" -O "/usr/share/ublue-os/just/81-bazzite-fixes.just"
 wget "https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/share/ublue-os/just/83-bazzite-audio.just" -O "/usr/share/ublue-os/just/83-audio.just"
