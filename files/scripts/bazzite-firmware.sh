91d6610da2047b8ff938ac618d67212b1f3a6d63
Ruben <sneexy@disroot.org>
2025-12-08 20:51:00 -0600

    fix(bazzite-firmware): add and remove bazzite repo as required

--- files/scripts/bazzite-firmware.sh
+++ files/scripts/bazzite-firmware.sh
@@ -9,4 +9,7 @@ set -x
 # Bazzite Firmware
 #
 # Downloads Bazzite's script for installing their own firmware during image building
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/build_files/install-firmware)"
+dnf5 -y copr enable bazzite-org/bazzite
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/build_files/install-firmware
+)"
+dnf5 -y copr remove bazzite-org/bazzite
