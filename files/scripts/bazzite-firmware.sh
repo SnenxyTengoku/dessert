f06a38ec163f65b034b2b06018323bd2d9d266d9
Ruben <sneexy@disroot.org>
2025-12-08 20:38:50 -0600

    feat: add bazzite firmware

---
+++ files/scripts/bazzite-firmware.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+# if shit breaks i at least know where it is
+set -x
+
+#
+# Bazzite Firmware
+#
+# Downloads Bazzite's script for installing their own firmware during image building
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/build_files/install-firmware)"
