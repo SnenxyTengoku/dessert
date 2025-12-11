3efffe23f420f85b6b904be2a6fe2a60266c02f0
Ruben <sneexy@disroot.org>
2025-12-11 16:08:59 -0600

    chore: migrate from flac2opus to flacconv

---
+++ files/scripts/additions/flacconv.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+set -euox pipefail
+
+#
+# flacconv
+#
+curl -sL -o /usr/bin/flacconv "https://git.unix.dog/yosh/flacconv/raw/branch/main/flacconv"
+chmod +x /usr/bin/flacconv
