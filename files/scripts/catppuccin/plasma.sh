34f03bfeae2fd49ac9479d7ea302795926075cb6
Ruben <sneexy@disroot.org>
2026-01-09 12:37:12 -0600

    feat(kde): add konsole catppuccin colorschemes

--- files/scripts/catppuccin/plasma.sh
+++ files/scripts/catppuccin/plasma.sh
@@ -12,3 +12,10 @@ chmod +x install.sh
 printf 'y\nn' | /tmp/catppuccinkde/install.sh 1 9 1
 # Mocha
 printf 'y\nn' | /tmp/catppuccinkde/install.sh 4 9 1
+
+#
+# Konsole
+#
+mkdir -p /etc/skel/.local/share/konsole
+curl -sL -o /etc/skel/.local/share/konsole/catppuccin-mocha.colorscheme https://raw.githubusercontent.com/catppuccin/konsole/refs/heads/main/themes/catppuccin-mocha.colorscheme
+curl -sL -o /etc/skel/.local/share/konsole/catppuccin-latte.colorscheme https://raw.githubusercontent.com/catppuccin/konsole/refs/heads/main/themes/catppuccin-latte.colorscheme
