251ea894e6fb143a08d2a323dc4bfe142e21f71d
Ruben <sneexy@disroot.org>
2026-02-10 16:07:25 -0600

    chore(mazurek): who would have guessed that vibecoded slop wouldn't work

--- files/scripts/build-scripts/wmutils.sh
+++ files/scripts/build-scripts/wmutils.sh
@@ -8,7 +8,15 @@ dnf install -y git gcc gcc-c++ cmake openssl protobuf protobuf-devel protobuf-co
 mkdir -p /artifacts/wmutils-built/usr/bin
 
 # Stasis - https://github.com/saltnpepper97/stasis
+#cd /tmp
+#git clone https://github.com/saltnpepper97/stasis.git
+#cd stasis
+#cargo install --path . --root /artifacts/wmutils-built/usr
+
+# SwayAudioIdleInhibit - https://github.com/ErikReider/SwayAudioIdleInhibit
 cd /tmp
-git clone https://github.com/saltnpepper97/stasis.git
-cd stasis
-cargo install --path . --root /artifacts/wmutils-built/usr
+git clone https://github.com/ErikReider/SwayAudioIdleInhibit
+cd SwayAudioIdleInhibit
+meson setup build -Dlogind-provider=systemd
+meson compile -C build --prefix /usr
+meson install -C build --destdir "/artifacts/wmutils-built"
