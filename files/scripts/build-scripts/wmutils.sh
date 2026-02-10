5655ecdf077c8edb2433b4ea91dde61fba2225df
Ruben <sneexy@disroot.org>
2026-02-10 16:33:35 -0600

    chore(mazurek): add other missing build dependencies for SwayAudioIdleInhibit

--- files/scripts/build-scripts/wmutils.sh
+++ files/scripts/build-scripts/wmutils.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 # dependencies
-dnf install -y git gcc gcc-c++ cmake meson pkgconf openssl protobuf protobuf-devel protobuf-compiler openssl-devel dbus-devel pkgconf-pkg-config rust-rustix-devel rustup cargo
+dnf install -y git gcc gcc-c++ cmake meson pkgconf openssl protobuf protobuf-devel protobuf-compiler openssl-devel dbus-devel pkgconf-pkg-config rust-rustix-devel systemd-devel pulseaudio-libs-devel pipewire-devel rustup cargo
 mkdir -p /artifacts/wmutils-built/usr/bin
 
 # Stasis - https://github.com/saltnpepper97/stasis
