e49059f280b5cc7d4b10ac2906e556958059e41d
Ruben <sneexy@disroot.org>
2026-02-12 20:46:19 -0600

    chore: clean up scripts, set mpdris2-rs's binary location correctly

--- files/scripts/base.sh
+++ files/scripts/base.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
-
 set -euox pipefail
 
 # ensure custom scripts are executable
 chmod +x /usr/bin/unfuck-music
 chmod +x /usr/bin/dns-toggle
+
+# MPD Discord RPC systemd service
+curl -sL -o /usr/lib/systemd/user/mpd-discord-rpc.service https://raw.githubusercontent.com/JakeStanger/mpd-discord-rpc/master/mpd-discord-rpc.service
+
+# Rescrobbled systemd service
+curl -sL -o /usr/lib/systemd/user/rescrobbled.service https://raw.githubusercontent.com/InputUsername/rescrobbled/master/rescrobbled.service
+sed -i 's@ExecStart=%h/.cargo/bin/rescrobbled@ExecStart=/usr/bin/rescrobbled@g' /usr/lib/systemd/user/rescrobbled.service # set correct bin location
+
+# mpdris2-rs systemd service
+curl -sL -o /usr/lib/systemd/user/mpdris2-rs.service https://raw.githubusercontent.com/szclsya/mpdris2-rs/refs/heads/trunk/misc/mpdris2-rs.service
+sed -i 's@ExecStart=/usr/local/bin/mpdris2-rs@ExecStart=/usr/bin/mpdris2-rs@g' /usr/lib/systemd/user/mpdris2-rs.service # set correct bin location
+sed -i 's@Wants=mpd.service@Wants=mpd.service noctalia.service@g' /usr/lib/systemd/user/mpdris2-rs.service # start it after noctalia shell starts to prevent startup issues
+
+# tlrc files
+curl -sL -o /usr/share/bash-completion/completions/tldr https://raw.githubusercontent.com/tldr-pages/tlrc/main/completions/tldr.bash
+curl -sL -o /usr/share/zsh/site-functions/_tldr https://raw.githubusercontent.com/tldr-pages/tlrc/main/completions/_tldr
+curl -sL -o /usr/share/fish/vendor_completions.d/tldr.fish https://raw.githubusercontent.com/tldr-pages/tlrc/main/completions/tldr.fish
+curl -sL -o /usr/share/man/man1/tldr.1 https://raw.githubusercontent.com/tldr-pages/tlrc/main/tldr.1
--- files/scripts/flavors/mazurek.sh
+++ files/scripts/flavors/mazurek.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-
 set -euox pipefail
-
-# Firefox customizations
-git clone https://github.com/soulhotel/FF-ULTIMA /etc/skel/.mozilla/firefox/default-profile/chrome # FF ULTIMA is our theme of choice, we provide a custom user.js based off of Betterfox merged with the themes'
-sed -i 's@rgb(70, 63, 77)@rgb(166, 227, 161)@g' /etc/skel/.mozilla/firefox/default-profile/chrome/theme/color-schemes/catppuccin-mocha/ffu-colorscheme.css # use green accent for catppuccin theme
 
 # Catppuccinify SDDM theme
 sed -i 's@TextColor="#F8F8F2"@TextColor="#cdd6f4"@g' /usr/share/sddm/themes/sddm-astronaut-theme/Themes/astronaut.conf
@@ -15,21 +10,3 @@ sed -i 's@PlaceholderColor="#bbbbbb"@PlaceholderColor="#6c7086"@g' /usr/share/sd
 sed -i 's@SystemButtonsIconColor="#ffffff"@SystemButtonsIconColor="#cdd6f4"@g' /usr/share/sddm/themes/sddm-astronaut-theme/Themes/astronaut.conf
 rm -f /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/1.png
 ln -sf /usr/share/backgrounds/catppuccin/black-hole-mocha.png /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/1.png
-
-# MPD Discord RPC systemd service
-curl -sL -o /usr/lib/systemd/user/mpd-discord-rpc.service https://raw.githubusercontent.com/JakeStanger/mpd-discord-rpc/master/mpd-discord-rpc.service
-
-# Rescrobbled systemd service
-curl -sL -o /usr/lib/systemd/user/rescrobbled.service https://raw.githubusercontent.com/InputUsername/rescrobbled/master/rescrobbled.service
-sed -i 's@ExecStart=%h/.cargo/bin/rescrobbled@ExecStart=/usr/bin/rescrobbled@g' /usr/lib/systemd/user/rescrobbled.service # set correct bin location
-sed -i 's@ExecStart=%h/.cargo/bin/rescrobbled@ExecStart=/usr/bin/rescrobbled@g' /usr/lib/systemd/user/rescrobbled.service # start it after noctalia shell starts to prevent startup issues
-
-# mpdris2-rs systemd service
-curl -sL -o /usr/lib/systemd/user/mpdris2-rs.service https://raw.githubusercontent.com/szclsya/mpdris2-rs/refs/heads/trunk/misc/mpdris2-rs.service
-sed -i 's@Wants=mpd.service@Wants=mpd.service noctalia.service@g' /usr/lib/systemd/user/mpdris2-rs.service
-
-# tlrc files
-curl -sL -o /usr/share/bash-completion/completions/tldr https://raw.githubusercontent.com/tldr-pages/tlrc/main/completions/tldr.bash
-curl -sL -o /usr/share/zsh/site-functions/_tldr https://raw.githubusercontent.com/tldr-pages/tlrc/main/completions/_tldr
-curl -sL -o /usr/share/fish/vendor_completions.d/tldr.fish https://raw.githubusercontent.com/tldr-pages/tlrc/main/completions/tldr.fish
-curl -sL -o /usr/share/man/man1/tldr.1 https://raw.githubusercontent.com/tldr-pages/tlrc/main/tldr.1
