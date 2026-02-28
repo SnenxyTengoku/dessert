ea84e0a5200de2af5f9ea158300af36cfd3cd77a
Ruben <sneexy@disroot.org>
2026-02-28 11:52:52 -0600

    chore(mazurek): noctalia became anti-woke and no longer supports systemd to launch it

--- files/scripts/flavors/mazurek.sh
+++ files/scripts/flavors/mazurek.sh
@@ -10,16 +10,13 @@ sed -i 's@SystemButtonsIconColor="#ffffff"@SystemButtonsIconColor="#cdd6f4"@g' /
 rm -f /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/1.png
 ln -sf /usr/share/backgrounds/catppuccin/black-hole-mocha.png /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/1.png
 
-# mpdris2-rs systemd service
-sed -i 's@Wants=mpd.service@Wants=mpd.service noctalia.service@g' /usr/lib/systemd/user/mpdris2-rs.service
-
 # add desktop shells and utilities to their systemd services as wants
-systemctl --global add-wants niri.service noctalia.service
-systemctl --global add-wants niri.service swayidle-niri.service
+#systemctl --global add-wants niri.service noctalia.service
+#systemctl --global add-wants niri.service swayidle-niri.service
 
 systemctl --global add-wants mangowc.target noctalia.service
 systemctl --global add-wants mangowc.target swayidle-mango.service
 systemctl --global add-wants mangowc.target xdg-desktop-autostart.target # allows autostart entries to actually start under mangowc
 
-systemctl --global add-wants noctalia.service mpdris2-rs.service # start mpdris2-rs after noctalia to prevent crashing for it starting before daemons are available
-systemctl --global add-wants noctalia.service syncthingtray.server # ensure syncthing tray starts after noctalia's tray is available
+#systemctl --global add-wants noctalia.service mpdris2-rs.service # start mpdris2-rs after noctalia to prevent crashing for it starting before daemons are available
+#systemctl --global add-wants noctalia.service syncthingtray.server # ensure syncthing tray starts after noctalia's tray is available
